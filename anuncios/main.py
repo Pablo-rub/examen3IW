4e7553bba909b4e0058e6879100db78b73ca53f4
Pablo <pabloruar03@uma.es>
2024-12-12 18:12:11 +0100

    main

--- anuncios/main.py
+++ anuncios/main.py
@@ -1,63 +1,51 @@
+from datetime import datetime
+from flask import Flask, request, redirect, url_for, jsonify
 import pymongo
-
 from environs import Env
 from bson import ObjectId
-from flask import Flask, render_template, request, redirect, url_for
-from datetime import datetime
 
 app = Flask(__name__)
 
 env = Env()
-env.read_env()  # read .env file, if it exists
+env.read_env()
 
 uri = env('MONGO_URI')
-
-print("MONGO_URI: ",uri)
-
+print("MONGO_URI: ", uri)
 client = pymongo.MongoClient(uri)
-
-db = client.examen3IW   # db = client['examen3IW']
-
-events = db.events      # events = db['events']
+db = client.examen3IW
+events = db.events
 
 # Definicion de metodos para endpoints
-
 @app.route('/', methods=['GET'])
 def showEvents():
-    
-    return render_template('events.html', events=list(events.find().sort('date', pymongo.DESCENDING)))
-    
-@app.route('/new', methods=['GET', 'POST'])
+    all_events = list(events.find().sort('date', pymongo.DESCENDING))
+    for event in all_events:
+        event['_id'] = str(event['_id'])
+    return jsonify({"events": all_events})
+
+@app.route('/new', methods=['POST'])
 def newEvent():
+    event = {
+        'author': request.form['inputAuthor'],
+        'text': request.form['inputText'],
+        'priority': int(request.form['inputPriority']),
+        'date': datetime.now()
+    }
+    events.insert_one(event)
+    return redirect(url_for('showEvents'))
 
-    if request.method == 'GET':
-        return render_template('new.html')
-    else:
-        event = {'author': request.form['inputAuthor'],
-                 'text': request.form['inputText'], 
-                 'priority': int(request.form['inputPriority']),
-                 'date': datetime.now()
-                }
-        events.insert_one(event)
-        return redirect(url_for('showEvents'))
-
-@app.route('/edit/<_id>', methods=['GET', 'POST'])
+@app.route('/edit/<_id>', methods=['POST'])
 def editEvent(_id):
-    
-    if request.method == 'GET':
-        event = events.find_one({'_id': ObjectId(_id)})
-        return render_template('edit.html', event=event)
-    else:
-        event = {'author': request.form['inputAuthor'],
-                 'text': request.form['inputText'],
-                 'priority': int(request.form['inputPriority'])
-                }
-        events.update_one({'_id': ObjectId(_id)}, {'$set': event})    
-        return redirect(url_for('showEvents'))
+    event = {
+        'author': request.form['inputAuthor'],
+        'text': request.form['inputText'],
+        'priority': int(request.form['inputPriority'])
+    }
+    events.update_one({'_id': ObjectId(_id)}, {'$set': event})
+    return redirect(url_for('showEvents'))
 
 @app.route('/delete/<_id>', methods=['GET'])
 def deleteEvent(_id):
-    
     events.delete_one({'_id': ObjectId(_id)})
     return redirect(url_for('showEvents'))
 
