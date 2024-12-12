13e84ab8bf28a31314bf95c83e037babf2756a38
Pablo <pabloruar03@uma.es>
2024-12-12 18:16:39 +0100

    vercel

--- anuncios/main.py
+++ anuncios/main.py
@@ -50,4 +50,4 @@ def deleteEvent(_id):
     return redirect(url_for('showEvents'))
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
